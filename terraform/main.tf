88209c9c66985d76cf2cc11cf3acd4d74fe0826e
Rohan-Biswal <rohanbiswal03@gmail.com>
2026-05-31 18:13:57 +0530

    Update function dependencies

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
 data "google_project" "project" {
   project_id = var.project_id
 }
+
+resource "google_storage_bucket" "function_bucket" {
+  name                        = "${var.project_id}-function-source"
+  location                    = var.region
+  uniform_bucket_level_access = true
+}
